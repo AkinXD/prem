b5829d626c7f9dc87210421bfd91023328102639
Samsudin <85529444+AkinXD@users.noreply.github.com>
2021-08-03 21:16:28 +0700

    Add files via upload

---
+++ pw/list_pass.py
@@ -0,0 +1,24 @@
+# CUIH rekod mlu
+# jangan rekod asuuuuuuuuuuuuuu!!!!!!!!!!!!!!!!!!!
+pw_tambahan="""
+
+name123|name12345|sayang|anjing|bangsat|bismillah|indonesia|rahasia
+
+""".strip().split("|")
+ngntd=((())>(()))+((())>(()))
+def awok(ngentod):
+	return [ngentod,ngentod+"123",ngentod+"12345"]
+def pw_list(ngentot):
+	if len(ngentot)==2:
+		aap_afandi=ngentot[1].split(" ")
+		if len(aap_afandi[ngntd])!=ngntd:
+			asu=awok(aap_afandi[ngntd])
+			if len(aap_afandi[ngntd])<=5:
+				del asu[ngntd]
+				for x in pw_tambahan: asu.append(x)
+			else:
+				for x in pw_tambahan: asu.append(x)
+		else: asu=pw_tambahan
+	else: asu=pw_tambahan
+	return asu
+ 
